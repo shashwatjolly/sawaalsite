6f7a4ccd3eeafc5501e62b53e3b2b4da2f3ccdbf
shaurya31 <shaurya.gomber98@iitg.ernet.in>
2019-12-18 12:21:39 +0530

    Confiure firebase

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const path = require('path');
+const firebase = require('firebase');
+
 const app = express();
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('./assets/'))
 const server = app.listen(7000, () => {
     console.log(`Express running → PORT ${server.address().port}`);
 });
+
+// Configuring firebase.
+var firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyAAqzctObFVSwj4YVwJh6NMARNTKqJCFFQ",
+    authDomain: "sawaal-sgsj.firebaseapp.com",
+    databaseURL: "https://sawaal-sgsj.firebaseio.com",
+    projectId: "sawaal-sgsj",
+    storageBucket: "sawaal-sgsj.appspot.com",
+    messagingSenderId: "1086466113456",
+    appId: "1:1086466113456:web:a4af7708fa679df8f4e075"
+});
+var databaseRef = firebaseApp.database();
+
 app.get('/', (req, res) => {
     res.render('index');
 });
