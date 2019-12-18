694d5488a8acb6ca35d3a6abe1681e7188fcd3bf
shaurya31 <shaurya.gomber98@iitg.ernet.in>
2019-12-18 15:41:37 +0530

    Implement Find Paper (without upvote/downvote)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const firebase = require('firebase');
+const bodyParser =  require('body-parser');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -21,9 +22,112 @@ var firebaseApp = firebase.initializeApp({
 });
 var databaseRef = firebaseApp.database();
 
+// To parse form data.
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+function getUserMail(){
+    return "shaurya.gomber98@iitg.ac.in";
+}
+
+function getPapersByYear(years,referString,res){
+    var locks = [];
+    var papers = [];
+
+    for(var i=0;i<years.length;i++){
+        locks.push(false);
+    }
+
+    var displayYear = years.length > 1; 
+
+    console.log(locks);
+    for(var i=0;i<years.length;i++){
+
+        var refString = `${referString}/${years[i]}`;
+        var paperRef = databaseRef.ref(refString);
+
+        const copy = i;
+        paperRef.orderByChild("totalVotes").once("value").then(function(snapshot,i) {
+            
+            snapshot.forEach(function(child) {
+                var paper = child.val();
+                paper.key = child.key;
+                papers.unshift(paper);
+            });
+
+            if(displayYear){
+                var yearObj = {};
+                yearObj.isYear = true;
+                yearObj.year = years[copy];
+                papers.unshift(yearObj);
+            }
+
+            locks[copy] = true;
+
+            var done = true;
+            for(var j=0;j<years.length;j++){
+                done = done && locks[j]; 
+                console.log(`${j},${locks[j]}`);
+            }
+
+            console.log(done);
+            if(done){
+                res.render('index', {"papers":papers});
+            }
+            
+            }, function (errorObject) {
+            console.log("The read failed: " + errorObject.code);
+        });
+    }
+}
+
 app.get('/', (req, res) => {
     res.render('form');
 });
+
+app.post('/', urlencodedParser, (req, res) => {
+    console.log(req.body);
+    const code = req.body.code;
+    const type = req.body.type;
+    const year = req.body.year;
+
+    var loggedInUser = getUserMail();
+    
+    if(year != ""){
+        var referString = `Uploads/${code}_${type}/${year}`;
+        var papersRef = databaseRef.ref(referString);
+        
+
+        papersRef.orderByChild("totalVotes").once("value").then(function(snapshot) {
+            var papers = [];
+            snapshot.forEach(function(child) {
+                var paper = child.val();
+                paper.key = child.key;
+                papers.unshift(paper);
+            });
+            res.render('index' , {"papers":papers});
+            }, function (errorObject) {
+            console.log("The read failed: " + errorObject.code);
+        });
+    
+    }
+    else{
+        var referString = `Uploads/${code}_${type}`;
+        var yearsRef = databaseRef.ref(referString);
+
+        const years = [];
+        yearsRef.once("value").then(function(snapshot) {
+            snapshot.forEach(function(child) {
+                years.push(child.key);
+            });
+            
+            console.log(years);
+            getPapersByYear(years,referString,res);
+            }, function (errorObject) {
+            console.log("The read failed: " + errorObject.code);
+        });
+    }
+});
+
 app.get('/index', (req, res) => {
     res.render('index');
 });
